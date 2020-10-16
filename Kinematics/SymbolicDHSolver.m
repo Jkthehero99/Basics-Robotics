fc6aefb54eb9ada14909844db14ac353289645d1
Jai Krishna <49020643+Jkthehero99@users.noreply.github.com>
2020-10-16 07:26:35 +0530

    latest

--- Kinematics/SymbolicDHSolver.m
+++ Kinematics/SymbolicDHSolver.m
@@ -1,6 +1,7 @@
 %%Symbolic solver
 noParams = input('Enter the number of axis: ');
 T = [1,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1];
+flg = [0,0,0,0];
 A = sym('A%d%d',[noParams,4]);
 for i=1:1:noParams
     Rz = [cos(A(i,1)),-sin(A(i,1)),0,0;sin(A(i,1)),cos(A(i,1)),0,0;0,0,1,0;0,0,0,1];
@@ -13,7 +14,54 @@ end
 
 %T = vpa(subs(T,A(3,1),0),4);
 
-for i=1:1:noParams 
+ch1 = input('Enter 1 if you have variables in your table, 2 for constants: ');
+if ch1 == 1
+    while flg(4) == 0
+        flg=input('Enter data as [position,value,1/0]: '); %constants filler
+        T = vpa(subs(T,A(flg(1),flg(2)),flg(3)),4);
+    end
+    Tpos=[T(1,4),T(2,4),T(3,4)];
+    %Tpost=Tpos;
+    Tpost = matlabFunction(Tpos);
+    var_index = input('Enter variable index: ');
+    elements = input('Enter lower:upper range: ');
+    %elements = {0, -pi/2:0.5:11*pi/18, -23*pi/18:0.5:5*pi/18, -20*pi/18:0.5:20*pi/18, -12*pi/18:0.5:12*pi/18, -40*pi/18:0.5:40*pi/18}; %cell array with N vectors to combine
+    combinations = cell(1, numel(elements)); %set up the varargout result
+    [combinations{:}] = ndgrid(elements{:});
+    combinations = cellfun(@(x) x(:), combinations,'uniformoutput',false); %there may be a better way to do this
+    result = [combinations{:}]; % NumberOfCombinations by N matrix. Each row is unique.
+    f(size(result,1),3) = 0;
+    disp('end length');
+    disp(size(result,1));
+    
+    for j=1:size(result,1)
+        f(j,:)=Tpost(result(j,1),result(j,2),result(j,3),result(j,4),result(j,5));
+    end
+    
+    disp('Done');
+    
+    
+    %%disp(result);
+%     for j=1:size(result,1)
+%         for k=1:size(result,2)
+%             
+%             %Tpost = vpa(subs(Tpost,A(k,var_index(1,k)),result(j,k)),4);
+%             
+%             %%disp('$');
+%         end
+%         %%disp('#');
+%         f(j,:)=simplify(Tpost);
+%         %disp(f);
+% %         plot3(f(1),f(2),f(3),'.r');
+% %         hold on
+%         if rem(j,5000)==0
+%             disp(j)
+%         end
+%         Tpost = Tpos;
+% end
+    
+elseif ch1 == 2
+    for i=1:1:noParams 
     inp = input('Enter teta, d , a , alpha: ');
     if size(inp,2) ~= 4 
         disp('Wrong data entry found, please restart the code');
@@ -21,6 +69,10 @@ for i=1:1:noParams
         for j=1:1:4
             T = vpa(subs(T,A(i,j),inp(j)),4);
         end
-     end
+    end
+    end
+    disp(T);    
+else
 end
-disp(T);
+
+
